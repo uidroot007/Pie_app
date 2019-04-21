a2998f2443745db687170ac68c1f84bef83845f6
uidroot007 <neo_the_one@outlook.com>
2019-04-21 20:02:57 +0200

    Updated

---
+++ Pie_app/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pie_app.Model;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pie_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        public readonly IPieRespo _pieRespo;
+
+        public HomeController(IPieRespo pieRespo)
+        {
+            _pieRespo = pieRespo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Pie overview";
+
+            var pies = _pieRespo.GetAllPies().OrderBy(p => p.Name);
+            return View(pies);
+        }
+    }
+}
---
+++ Pie_app/Model/IPieRespo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pie_app.Model
+{
+    public interface IPieRespo
+    {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
+    }
+}
